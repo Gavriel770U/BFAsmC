921a9aa4a96013bf99eebd454a463c4b5986c4d5
Gavriel770U <kraserultra@gmail.com>
2023-11-13 17:54:24 +0200

    Add read_file function

--- cmplr.asm
+++ cmplr.asm
@@ -6,11 +6,18 @@ DATASEG
     MEMORY_SIZE equ 100
     INITIAL_MEMORY_VALUE equ 0
 
+    COMMANDS_SIZE equ 2000
+    INITIAL_COMMAND_SIZE equ 0
+
     file_name db 'code.bf', 0
     file_handle dw ?
     error_message db 'Error', 10, 13, '$'
 
+    commands_length dw 0
+
     memory db MEMORY_SIZE dup(INITIAL_MEMORY_VALUE)
+
+    commands db COMMANDS_SIZE dup(INITIAL_COMMAND_SIZE)
 
 CODESEG
 
@@ -81,6 +88,45 @@ proc close_file
 endp close_file
 ;----------------------------------------------------------------
 
+;----------------------------------------------------------------
+proc read_file
+    ; [bp+4] offset file_handle
+    ; [bp+6] offset commands
+    ; [bp+8] offset commands_length
+
+    push bp
+    mov bp, sp
+    push ax
+    push bx
+    push cx
+    push dx
+
+    xor ax, ax
+    xor bx, bx
+    xor cx, cx
+    xor dx, dx
+
+    mov ah, 3Fh
+    mov bx, [bp+4]
+    mov bx, [bx]
+    mov cx, COMMANDS_SIZE
+    mov dx, [bp+6]
+    int 21h
+
+    ; ax stores the length of 
+    ; the commands buffer
+    mov bx, [bp+8]
+    mov [bx], ax
+
+    pop dx
+    pop cx
+    pop bx
+    pop ax
+    pop bp
+    ret 6
+endp read_file
+;----------------------------------------------------------------
+
 main:
     mov ax, @data
     mov ds, ax
@@ -90,6 +136,11 @@ main:
     push offset file_handle
     push offset file_name
     call open_file
+
+    push offset commands_length
+    push offset commands
+    push offset file_handle
+    call read_file
 
     push offset file_handle
     call close_file
